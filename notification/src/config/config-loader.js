4ae6c45dfe72b4d6689c3ed76c89208e8295ebd6
sfedyna <Popil1990>
2024-02-15 08:25:46 +0200

    Fixed problem with config

--- notification/src/config/config-loader.js
+++ notification/src/config/config-loader.js
@@ -1,7 +1,7 @@
 import rc from 'rc'
 
 function loadConfig() {
-  return JSON.parse("{\"commercetools\": {\"checkout-project\": {\"clientId\": \"pEKakGQCx3CjBH8L60F3rNpE\", \"clientSecret\": \"ccAd2pxnP9oRjj_Hc0SvYqPln83Hw2sy\"}}, \"adyen\": {\"JSPAccount417ECOM\": {\"apiKey\":\"AQEvhmfxLYPOaRNHw0m/n3Q5qf3VYrh9LJBJV3BY0iz6yR9HXFrFu9khnAlop6sHZ10QwV1bDb7kfNy1WIxIIkxgBw==-+jF4BCTOz4329NqMpX+4qjhYVzNEfPhGeqRe5A2mWGM=-)5aCYjWfkQA58S7G\"}}, \"adyenPaymentMethodsToNames\": {\"scheme\": {\"en\": \"Credit Card\"}, \"pp\": {\"en\": \"PayPal\"}, \"klarna\": {\"en\": \"Klarna\"}, \"gpay\": {\"en\": \"Google Pay\"}, \"affirm\": {\"en\": \"Affirm\"}}}");
+  return JSON.parse("{\"commercetools\": {\"checkout-project\": {\"clientId\": \"pEKakGQCx3CjBH8L60F3rNpE\", \"clientSecret\": \"ccAd2pxnP9oRjj_Hc0SvYqPln83Hw2sy\"}}, \"adyen\": {\"JSPAccount417ECOM\": {\"enableHmacSignature\":\"false\"}}, \"port\": \"8081\"}");
 
   if (process.env.ADYEN_INTEGRATION_CONFIG) {
     return loadFromAdyenIntegrationEnvVar()
